8ef145a90ab5efdfe35afca51104703ad90b2d43
Jack Toland <tolandjl@gmail.com>
2019-11-30 10:17:04 -0800

    Use ODE45 to drive nominal case

--- ASEN5044_HW8_problem2.m
+++ ASEN5044_HW8_problem2.m
@@ -12,6 +12,8 @@ r0 = 6678;          % km
 x0 = [6678, 0, 0, r0*sqrt(mu/r0^3)]';
 P = 2*pi*sqrt(r0^3/mu);
 
+t_vec = 0:10:P;
+
 %====== a ======%
 
 r0_nom = 6678;
@@ -32,6 +34,10 @@ Btil = [0, 0;
 
 
 %====== b ======%
+s0 = x0;
+opts = odeset('RelTol',1e-12,'AbsTol',1e-12);
+[T, S] = ode45(@(t,s)orbit_prop_func(t,s),t_vec,s0,opts);
+
 dt = 10;
 
 % Nominalize the A matrix
@@ -43,7 +49,7 @@ updatex = x0;
 x_sim = [];
 for i = 1:length(t_vec)
         x_sim = horzcat(x_sim, updatex);
-        F = F_variant(updatex(1),updatex(3),dt);
+        F = F_variant(S(i,1),S(i,3));
         updatex = F*updatex;
 end
 x_sim = x_sim';
@@ -55,9 +61,7 @@ x_sim = x_sim';
 % Simulate the Linearized DT dynamics
 
 
-s0 = x0 + [1, 1, 0.1, 0.1]';
-opts = odeset('RelTol',1e-12,'AbsTol',1e-12);
-[T, S] = ode45(@(t,s)orbit_prop_func(t,s),[0 P],s0,opts);
+
 
 
 fprintf('Plotting Satellite for 1 Orbit:\n');
@@ -99,10 +103,11 @@ saveas(fig,'ASEN5044_HW8_P2_sim.png','png');
 
 
 
-function [ F ] = F_variant(X,Y,dt)
+function [ F ] = F_variant(X,Y)
 
 mu = 398600;        % km^3/s^2
 r0_nom = 6678;          % km
+dt = 10;
 
 F = expm(dt*[0, 1, 0, 0;
         (-mu/r0_nom^3)+((3*mu*X^2)/r0_nom^5), 0, ((3*mu*X*Y)/r0_nom^5), 0;
@@ -110,8 +115,6 @@ F = expm(dt*[0, 1, 0, 0;
         ((3*mu*X*Y)/r0_nom^5), 0, (-mu/r0_nom^3)+((3*mu*Y^3)/r0_nom^5), 0]);
 
 end
-
-
 
 
 function [ ds ] = orbit_prop_func(t,s)
