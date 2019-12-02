42340fc5818498b744b15e5a4cf473df6a7cf588
Jack Toland <tolandjl@gmail.com>
2019-12-01 16:02:16 -0800

    Updated dx0

--- ASEN5044_HW8_problem2.m
+++ ASEN5044_HW8_problem2.m
@@ -43,7 +43,7 @@ dt = 10;
 % Nominalize the A matrix
 delta_x = [0 0 0 0]';
 
-dx = [0.01, 0.001, 0.01, 0.001]';
+dx = [0.01, 0.01, 0.01, 0.01]';
 dx_lin = [];
 for i = 1:length(t_vec)
     dx_lin = horzcat(dx_lin, dx(:,i));
